da9bf382764c63bae8105ebe40a98ab24cd958c4
Nandakumar.M <88134258+nandurpm@users.noreply.github.com>
2026-06-27 00:22:06 +0530

    Add cleanup for unavailable action placeholders

---
+++ assets/js/hide-unavailable-actions.js
@@ -0,0 +1,20 @@
+(() => {
+  "use strict";
+
+  function clean(root = document) {
+    root.querySelectorAll(".availability-label").forEach((element) => element.remove());
+  }
+
+  clean();
+  document.addEventListener("DOMContentLoaded", () => clean(), { once: true });
+
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeType === Node.ELEMENT_NODE) clean(node);
+      });
+    }
+  });
+
+  observer.observe(document.documentElement, { childList: true, subtree: true });
+})();
