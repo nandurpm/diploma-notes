1ff805b08016f15b019799dc1ecc925031faccf6
Nandakumar.M <88134258+nandurpm@users.noreply.github.com>
2026-06-27 11:44:21 +0530

    Load Ask POLY UI polish layer

--- assets/js/ask-poly-config.js
+++ assets/js/ask-poly-config.js
@@ -3,3 +3,13 @@ globalThis.ASK_POLY_CONFIG = Object.freeze({
   timeoutMs: 30000,
   maxHistory: 12
 });
+
+(() => {
+  if (!/\/ask-poly\.html$/i.test(location.pathname)) return;
+  if (document.getElementById("ask-poly-ui-polish-loader")) return;
+  const script = document.createElement("script");
+  script.id = "ask-poly-ui-polish-loader";
+  script.src = "/assets/js/ask-poly-ui-polish.js?v=20260627-ui-polish";
+  script.defer = true;
+  document.head.append(script);
+})();
