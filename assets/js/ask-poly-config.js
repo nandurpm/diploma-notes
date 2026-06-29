3a379d7b47bdcc22f56278e517aa95819df4cb21
Nandakumar.M <88134258+nandurpm@users.noreply.github.com>
2026-06-29 08:36:34 +0530

    Load Ask POLY knowledge layer

--- assets/js/ask-poly-config.js
+++ assets/js/ask-poly-config.js
@@ -3,3 +3,19 @@ globalThis.ASK_POLY_CONFIG = Object.freeze({
   timeoutMs: 30000,
   maxHistory: 12
 });
+
+(() => {
+  if (!/\/ask-poly\.html$/i.test(location.pathname)) return;
+  const files = [
+    ["ask-poly-knowledge-loader", "/assets/js/ask-poly-knowledge-loader.js?v=20260629-knowledge"],
+    ["ask-poly-ui-polish-loader", "/assets/js/ask-poly-ui-polish.js?v=20260629-ui-polish"]
+  ];
+  files.forEach(([id, src]) => {
+    if (document.getElementById(id)) return;
+    const script = document.createElement("script");
+    script.id = id;
+    script.src = src;
+    script.defer = true;
+    document.head.append(script);
+  });
+})();
