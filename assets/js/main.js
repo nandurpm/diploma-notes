2836cb423ac00b2005a690f49fddef391d38cd2c
nandurpm <8.8134258e+07+nandurpm@users.noreply.github.com>
2026-08-19 06:24:01 +0000

    fix: refresh pre-onam controller asset cache

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,7 +247,7 @@
     if (existing) return;
 
     const script = document.createElement("script");
-    script.src = `${themePath}?v=20260819-pre-onam-awesome1`;
+    script.src = `${themePath}?v=23f3d9df3386`;
     script.defer = true;
     script.dataset.polyPreOnamTheme = "true";
     document.head.append(script);
