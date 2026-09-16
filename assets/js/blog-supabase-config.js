7a1c8df4ccda40ec535421b142be1b677d8ecfe6
Nandakumar.M <88134258+nandurpm@users.noreply.github.com>
2026-09-16 16:40:14 +0530

    feat(blog): add Supabase publishing config

---
+++ assets/js/blog-supabase-config.js
@@ -0,0 +1,9 @@
+(() => {
+  "use strict";
+  window.POLY_BLOG_CONFIG = Object.freeze({
+    supabaseUrl: "https://hwobooljdvynsajtrvnk.supabase.co",
+    publishableKey: "sb_publishable_D8iv2EsMjr3VBzoDXkI7-w_1rWobLMD",
+    postsTable: "blog_posts",
+    imageBucket: "blog-images"
+  });
+})();
